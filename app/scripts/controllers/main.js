e5f1be85ab6d2fed2a6ca15a21f4cc522c5ff667
Evelyn-McDonald <evel.mcdonald@gmail.com>
2015-08-06 13:33:58 -0400

    filtering fixed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,14 +10,20 @@
 angular.module('starWarsApp')
   .controller('MainCtrl', function ($scope, $rootScope) {
 
-  	//	Set current person before switching to details view
-  	// $scope.setPerson = function(p) {
-  	// 	$rootScope.person = p;
-  	// };
-
-  	// $scope.getDetails = function(p) {
-  	// 	var index = $rootScope.people
-  	// }
   	void($scope, $rootScope);
+
+  	$scope.orderByInt = function(field) {
+  		var i = parseInt(field.replace(/\D/g,''));
+  		//var i = parseInt(field);
+  		return (i ? i : Number.POSITIVE_INFINITY);
+  	};
+
+  	$scope.orderByBirthYear = function(p) {
+  		return $scope.orderByInt(p.birth_year);
+  	};
+
+  	$scope.orderByMass = function(p) {
+  		return $scope.orderByInt(p.mass);
+  	};
 
   });
